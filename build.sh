58e66797c84d2f191c1a204779edda7732f8b0eb
Nilber <nilber@vittorazzi.com>
2020-01-20 19:17:37 -0300

    bkp

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ DOCKER_IMAGEM=laravel-alpine
 S2I_IMAGEM=nbmaster/openshift-laravel-alpine
 TAG=1.0
 
-docker system prune
-docker rmi $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
+# docker system prune
+# docker rmi $DOCKER_IMAGEM:$TAG $S2I_IMAGEM:$TAG
 
 docker build src/ -t $DOCKER_IMAGEM:$TAG
 
